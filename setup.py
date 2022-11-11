190a3d40382a033ebf4339e7a32faf4becf98f65
lfo96 <117573474+lfo96@users.noreply.github.com>
2022-11-11 23:51:00 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 # Process and convert files from BioLogic's EC-lab https://pypi.org/project/eclabfiles/
 import galvani 
-import general_functions
-import GCPL_functions
-import impedance_functions
-import three_el_functions
-import auto
+
+import Python_scripts.general_functions
+import Python_scripts.GCPL_functions
+import Python_scripts.impedance_functions
+import Python_scripts.three_el_functions
+import Python_scripts.auto

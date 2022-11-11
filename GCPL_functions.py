b3883fb11b0924e050198716c6b9f7e97a1a8eb1
lfo96 <117573474+lfo96@users.noreply.github.com>
2022-11-11 23:40:31 +0000

    Update GCPL_functions.py

--- GCPL_functions.py
+++ GCPL_functions.py
@@ -3,7 +3,7 @@ np.seterr(divide='ignore', invalid='ignore')
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
-import gspread
+#import gspread
 
 from Python_scripts.general_functions import info
 from Python_scripts.general_functions import layout
@@ -476,6 +476,7 @@ def datetime_sec(string):
 def eva_nda(pathway,m_am=np.nan,A_el=np.nan,data_log = False):
     '''
     Insert pathway of .nda file, active material mass and electrode surface area of nda file.
+    # Important: Only set data_log on if you want to read out from Google form. Requires gspread.
     '''
     df = read_nda(pathway)
     meta = {}

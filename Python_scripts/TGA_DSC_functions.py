6ee3b3ff70c006e122ba2254cc0ee29b625c3bf2
lfo96 <117573474+lfo96@users.noreply.github.com>
2022-11-12 13:37:06 +0000

    add XPS and TGA

---
+++ Python_scripts/TGA_DSC_functions.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import time
+import numpy as np
+import datetime as dt
+
+from itertools import islice
+from Python_scripts.general_functions import layout
+
+def TGA_DSC(pathway, plot = True, save = '', x_lim='', y_lim ='', y2_lim = '', m_real = ''):
+    with open(pathway,encoding= 'unicode_escape') as fin:
+        head_len = 34
+        for line in islice(fin, 0,head_len):
+                #print(line.split(":")[0])
+                if line.split(":")[0]=="#SAMPLE MASS /mg": #extract the active material mass
+                    m_am = float(line.split(",")[1][0:5])
+    
+    df = pd.read_csv(pathway,encoding= 'unicode_escape', header=34)
+    df['Mass loss (mg)'] = ((df['Mass/%'].iloc[0]-df['Mass/%'])/100)*m_am*-1
+    if m_real != '':
+        df['Mass corrected (%)'] = 100-((df['Mass loss (mg)']/m_real)*-100)
+    
+
+    meta = {
+        'sample mass' : m_am,
+        'data' : df   
+    }
+
+    if plot == True:
+        if m_real != '':
+            plot_TGA_DSC(meta['data'], save = save, correction = True, x_lim = x_lim, y_lim = y_lim, y2_lim = y2_lim)
+        else:
+            plot_TGA_DSC(meta['data'], save = save, x_lim = x_lim, y_lim = y_lim, y2_lim = y2_lim)
+
+
+    return(meta)
+
+def plot_TGA_DSC(data, save = '', correction = False, x_lim= '', y_lim = '', y2_lim = ''):
+    fig,ax = plt.subplots()
+    if correction == True:
+        ax.plot(data['##Temp./C'],data['Mass corrected (%)'], color = 'black')
+    else:
+        ax.plot(data['##Temp./C'],data['Mass/%'], color = 'black')
+    
+    #plt.axis('off')
+
+    if x_lim != '':
+        layout(ax,x_lim=x_lim,x_label='Temperature (\N{DEGREE SIGN}C)', y_label='Mass (%)')
+    if y_lim != '':
+        layout(ax,y_lim=y_lim,x_label='Temperature (\N{DEGREE SIGN}C)', y_label='Mass (%)')
+    else:
+        layout(ax,x_label='Temperature (\N{DEGREE SIGN}C)', y_label='Mass (%)')
+
+    color_ax2 = 'blue'
+    ax2 = ax.twinx()
+    ax2.set_ylabel('DSC', color=color_ax2,fontsize = 16)  # we already handled the x-label with ax1
+    ax2.tick_params(axis='y', labelcolor=color_ax2)
+    ax2.tick_params(direction='in', length=6, width=1.5, color = color_ax2)
+    ax2.spines["right"].set_color(color_ax2)
+    figure = plt.gca()
+    y_axis = figure.axes.get_yaxis()
+    y_axis.set_visible(False)
+    ax2.plot(data['##Temp./C'],data['DSC/(uV/mg)']*-1, color = color_ax2)
+    if y2_lim == '':
+        layout(ax2)
+    else:
+        layout(ax2, y_lim = y2_lim)
+    if save != '':
+        plt.savefig(save+'.svg')
+    layout(ax2)
+    
+
---
+++ Python_scripts/XPS_functions.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+from scipy.integrate import simps
+
+def eva_XPS(pathway):
+    df = pd.read_csv(pathway, sep='\t', header = 5)
+    dict = {}
+    spectrum_index = []
+    for i,entry in enumerate(df):
+        # extract index where a new spectrum starts.
+        if 'B.E.' in entry:
+            spectrum_index.append(i)
+
+    data = []
+    # create new DF for each spectrum
+    for index_i in range(len(spectrum_index)):
+        if index_i != len(spectrum_index)-1:
+            df_spec = df[df.columns[spectrum_index[index_i]:spectrum_index[index_i+1]]]
+        else:
+            df_spec = df[df.columns[spectrum_index[index_i]:]]  
+        data.append(df_spec)  
+
+    dict['data'] = data
+
+    # Evaluate the file. To be edited...
+    eva = []
+    for spectrum in dict['data']:
+        dict_s = {}
+        dict_data = {}
+        areas = {}
+        name = spectrum.columns[1]
+        cycle = name.split(':')[0]
+        element = name.split(':')[3]
+        BE = spectrum[spectrum.columns[0]]
+        raw = spectrum[spectrum.columns[1]]
+        raw_norm =  spectrum[spectrum.columns[1]]/spectrum[spectrum.columns[1]].max()
+        
+        dict_data['Binding Energy (eV)'] = BE
+        dict_data['Counts (1/s)'] = raw
+        dict_data['Counts norm'] = raw_norm
+        curves = []
+        #add remaining coloumns which will appear when the file is processed.
+        for col in spectrum.columns[2:]:
+            col_name = col.split(':')[-1]
+            if np.isnan(spectrum[col]).all() == True:
+                continue
+            elif 'Background' in col_name:
+                dict_data['Background'] = spectrum[col]
+            elif 'Envelope' in col_name:
+                dict_data['Envelope'] = spectrum[col]
+                envelope_A = simps((dict_data['Envelope']-dict_data['Background']).dropna(),-dict_data['Binding Energy (eV)'].dropna())
+                areas['Envelope'] = envelope_A
+            else:
+                dict_data[col_name] = spectrum[col]
+                curves.append(col_name)
+        
+        for curve in curves:
+            BL_sub = dict_data[curve] - dict_data['Background']
+            curve_A = simps(BL_sub.dropna(),-dict_data['Binding Energy (eV)'].dropna())
+            areas[curve] = curve_A
+            
+        dict_s['name'] = name
+        dict_s['cycle'] = cycle
+        dict_s['element'] = element
+        dict_s['data'] = pd.DataFrame(dict_data)
+        dict_s['area'] = areas
+        
+
+        eva.append(dict_s)
+        
+    dict['eva'] = eva
+    
+    return(dict)

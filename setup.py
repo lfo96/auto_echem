7a74f8f85bbaa8a588d75767ede00f5b90568758
lfo96 <117573474+lfo96@users.noreply.github.com>
2022-11-12 16:46:26 +0000

    make it work

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# Process and convert files from BioLogic's EC-lab https://pypi.org/project/eclabfiles/
 from setuptools import setup
 
 setup(
@@ -12,12 +11,3 @@ setup(
         'gspread'
     ]
 )
-
-# pip install 
-# import scipy
-# import galvani 
-# print('Successfully imported galvani.')
-# ## EISArt for impedance analysis https://github.com/leehangyue/EISART
-# ## Authors for citation when EISArt used in scientific work.
-# #import EISART.code.EISART
-# #print('Successfully imported EISArt.')

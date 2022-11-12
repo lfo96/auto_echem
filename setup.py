a6875e737241dddf5e5c675d21b192972072f91d
lfo96 <117573474+lfo96@users.noreply.github.com>
2022-11-12 11:45:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 # Process and convert files from BioLogic's EC-lab https://pypi.org/project/eclabfiles/
 import galvani 
+
+import EISART.quadprof-0.1.11.setup
+
+# EISArt for impedance analysis https://github.com/leehangyue/EISART
+# Authors for citation when EISArt used in scientific work.
 import EISART.code.EISART
 import Python_scripts.general_functions
 import Python_scripts.GCPL_functions

7a24dceb318fc0428c04840c6226a80d8a2fe18b
lfo96 <117573474+lfo96@users.noreply.github.com>
2022-11-13 13:59:10 +0000

    modify

---
+++ code/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Auto EChem',
+    version='0.0.1',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'galvani',
+        'impedance',
+        'gspread'
+    ]
+)

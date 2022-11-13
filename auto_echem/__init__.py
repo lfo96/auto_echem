4203b5dc2bd15a2841127fa1d6dc317cd9f2fe0b
lfo96 <117573474+lfo96@users.noreply.github.com>
2022-11-13 18:33:20 +0000

    Update __init__.py

--- auto_echem/__init__.py
+++ auto_echem/__init__.py
@@ -1,1 +1,8 @@
 # create package
+from auto_echem.GCPL_functions import *
+from auto_echem.general_functions import *
+from auto_echem.three_el_functions import *
+from auto_echem.auto import *
+from auto_echem.impedance_functions import *
+from auto_echem.TGA_DSC_functions import *
+from auto_echem.XPS_functions import *

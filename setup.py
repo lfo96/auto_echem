acbc919555d96c5cb72bccf80074cc3765b3685c
lfo96 <117573474+lfo96@users.noreply.github.com>
2022-11-13 14:27:52 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,12 @@ from setuptools import setup
 setup(
     name='Auto EChem',
     version='0.0.1',
+    packages=['Python_script'],
     install_requires=[
         'numpy',
         'scipy',
         'galvani',
         'impedance',
         'gspread',
-        py_modules = [
-            'Python_scripts\auto',
-            'Python_scripts\GCPL_functions',
-            'Python_scripts\impedance_functions',
-            'Python_scripts\general_functions',
-            'Python_scripts\TGA_DSC_functions',
-            'Python_scripts\three_el_functions',
-            'Python_scripts\XPS_functions',
-            ],
     ]
 )
